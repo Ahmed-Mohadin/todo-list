729d059584038647b9a2fd99aa750ea5dfaf4ee7
ahmed.mohadin <ahmed.mohadin@outlook.com>
2022-03-19 22:01:21 +0100

    Design update, added close and open nav icon

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ var __webpack_exports__ = {};
   !*** ./src/index.js ***!
   \**********************/
 
+document.querySelector('.menu').addEventListener('click', () => {
+    navWidth('16rem');    
+});
+
+document.querySelector('.fa-xmark').addEventListener('click', () => {
+    navWidth('0rem');
+})
+
+function navWidth(width){
+    document.querySelector('.project-collection').style.width = `${width}`;
+}
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBLENBQUM7QUFDRDtBQUNBO0FBQ0E7QUFDQSxDQUFDO0FBQ0Q7QUFDQTtBQUNBLG1FQUFtRSxNQUFNO0FBQ3pFLEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiXHJcbmRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJy5tZW51JykuYWRkRXZlbnRMaXN0ZW5lcignY2xpY2snLCAoKSA9PiB7XHJcbiAgICBuYXZXaWR0aCgnMTZyZW0nKTsgICAgXHJcbn0pO1xyXG5cclxuZG9jdW1lbnQucXVlcnlTZWxlY3RvcignLmZhLXhtYXJrJykuYWRkRXZlbnRMaXN0ZW5lcignY2xpY2snLCAoKSA9PiB7XHJcbiAgICBuYXZXaWR0aCgnMHJlbScpO1xyXG59KVxyXG5cclxuZnVuY3Rpb24gbmF2V2lkdGgod2lkdGgpe1xyXG4gICAgZG9jdW1lbnQucXVlcnlTZWxlY3RvcignLnByb2plY3QtY29sbGVjdGlvbicpLnN0eWxlLndpZHRoID0gYCR7d2lkdGh9YDtcclxufSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
